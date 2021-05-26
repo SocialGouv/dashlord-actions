9aea2184f4a139fb905ffef43ac5be9a9454d755
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-05-27 01:51:18 +0200

    chore: extract generateUrlReport and tests

---
+++ report/src/generateUrlReport.js
@@ -0,0 +1,231 @@
+const fs = require("fs");
+const path = require("path");
+const pick = require("lodash.pick");
+const omit = require("lodash.omit");
+const YAML = require("yaml");
+
+const { readFile, writeFile } = require("./utils");
+
+const DASHLORD_REPO_PATH = process.env.DASHLORD_REPO_PATH || ".";
+
+/**
+ * Try to require some JSON
+ *
+ * @param {string} jsonPath The full path to file
+ *
+ * @returns {any} JSON content
+ */
+const requireJson = (jsonPath) => {
+  try {
+    return require(jsonPath);
+  } catch (e) {
+    console.error("e", e);
+    return null;
+  }
+};
+
+/**
+ * Minify zap JSON data
+ *
+ * @param {ZapReport} result ZAP JSON content
+ *
+ * @returns {ZapReport} minified JSON content
+ */
+const zapCleanup = (result) =>
+  result && {
+    ...result,
+    site:
+      result &&
+      result.site &&
+      result.site.map &&
+      result.site.map((site) => {
+        return {
+          ...site,
+          alerts: site.alerts.map((result) =>
+            pick(result, ["name", "riskcode", "confidence", "riskdesc", "desc"])
+          ),
+        };
+      }),
+  };
+
+/**
+ * Minify nuclei JSON data
+ *
+ * @param {NucleiReport} result nuclei JSON content
+ * @param {string} url extract only for this url
+ *
+ * @returns {NucleiReport} minified JSON content
+ */
+const nucleiCleanup = (result, url) =>
+  result &&
+  result.map &&
+  result
+    .map((r) => omit(r, ["request", "response"]))
+    .filter((entry) => entry.host === url);
+
+/**
+ * Minify Lighthouse JSON data
+ *
+ * @param {LighthouseReport} result Lighthouse JSON content
+ *
+ * @returns {LighthouseReport|null} minified JSON content
+ */
+const lhrCleanup = (result) => {
+  if (!result) {
+    return null;
+  }
+  const {
+    requestedUrl,
+    finalUrl,
+    fetchTime,
+    runWarnings,
+    categories,
+    audits,
+  } = result;
+
+  /** @type {LighthouseReportCategories} */
+  // @ts-ignore
+  const newCategories =
+    (categories &&
+      Object.keys(categories).reduce(
+        (
+          a,
+          /** @type {LighthouseReportCategoryKey} */
+          key
+        ) => ({
+          ...a,
+
+          [key]: omit(categories[key], "auditRefs"),
+        }),
+        {}
+      )) ||
+    {};
+  return {
+    requestedUrl,
+    finalUrl,
+    fetchTime,
+    runWarnings,
+    categories: newCategories,
+    audits: pick(audits, ["metrics", "diagnostics"]),
+  };
+};
+
+const cleanups = {
+  nuclei: nucleiCleanup,
+  zap: zapCleanup,
+  lhr: lhrCleanup,
+};
+
+//@ts-expect-error
+const requireToolData = (filename) => (basePath) =>
+  requireJson(path.join(basePath, filename));
+
+const tools = {
+  http: { data: requireToolData("http.json") },
+  updownio: { data: requireToolData("updownio.json") },
+  nmap: { data: requireToolData("nmapvuln.json") },
+  dependabot: { data: requireToolData("dependabotalerts.json") },
+  codescan: { data: requireToolData("codescanalerts.json") },
+  testssl: { data: requireToolData("testssl.json") },
+  thirdparties: { data: requireToolData("thirdparties.json") },
+  wappalyzer: { data: requireToolData("wappalyzer.json") },
+  zap: { data: requireToolData("zap.json"), cleanup: zapCleanup },
+  nuclei: { data: requireToolData("nuclei.json"), cleanup: nucleiCleanup },
+  lhr: { data: requireToolData("lhr.json"), cleanup: lhrCleanup },
+  screenshot: {
+    //@ts-expect-error
+    data: (basePath) => fs.existsSync(path.join(basePath, "screenshot.jpeg")),
+  },
+};
+
+//@ts-expect-error
+const noop = (args) => args;
+
+/**
+ * Compile report for a single URL
+ *
+ * @param {UrlConfig} url URL to compile results for
+ *
+ * @returns {UrlReport|null} compiled data
+ */
+const generateUrlReport = (url) => {
+  const urlb64 = Buffer.from(url.url).toString("base64");
+  const urlPath = path.join(DASHLORD_REPO_PATH, "results", urlb64);
+  if (fs.existsSync(urlPath)) {
+    // use filesystem to determine latest scan report
+    const scans = fs.readdirSync(urlPath);
+    scans.sort().reverse();
+    const lastScan = scans.length && scans[0];
+    if (!lastScan) {
+      return null;
+    }
+    const latestFilesPath = path.join(urlPath, lastScan);
+    const urlData = {
+      ...url,
+      ...Object.keys(tools).reduce((keys, key) => {
+        //@ts-expect-error
+        const data = (tools[key].cleanup || noop)(
+          //@ts-expect-error
+          tools[key].data(latestFilesPath, url),
+          url
+        );
+        return {
+          ...keys,
+          [key]: data,
+        };
+      }, {}),
+    };
+
+    // copy lhr, zap and testssl.sh static reports
+    const publicReportsUrlPath = path.join("www", "public", "report", urlb64);
+
+    fs.mkdirSync(publicReportsUrlPath, { recursive: true });
+
+    /** @type {DashlordConfig} */
+    let dashlordConfig = {
+      title: "DashLord report",
+      urls: [],
+    };
+    if (fs.existsSync(path.join(DASHLORD_REPO_PATH, "dashlord.yaml"))) {
+      dashlordConfig = YAML.parse(
+        readFile(path.join(DASHLORD_REPO_PATH, "dashlord.yaml"))
+      );
+    } else if (fs.existsSync(path.join(DASHLORD_REPO_PATH, "dashlord.yml"))) {
+      dashlordConfig = YAML.parse(
+        readFile(path.join(DASHLORD_REPO_PATH, "dashlord.yml"))
+      );
+    }
+
+    // copy dashlord config YAML as JSON for the report
+    writeFile(
+      path.join("www", "src", "config.json"),
+      JSON.stringify(dashlordConfig)
+    );
+
+    /**
+     *
+     * @param {string} name file name to export to public website
+     *
+     * @returns {void}
+     */
+    const copyForWebsite = (name) => {
+      if (fs.existsSync(path.join(latestFilesPath, name))) {
+        fs.createReadStream(path.join(latestFilesPath, name)).pipe(
+          fs.createWriteStream(path.join(publicReportsUrlPath, name))
+        );
+      }
+    };
+
+    copyForWebsite("lhr.html");
+    copyForWebsite("testssl.html");
+    copyForWebsite("zap.html");
+    copyForWebsite("screenshot.jpeg");
+
+    return urlData;
+  } else {
+    console.error(`Cannot find folder for ${url.url}`);
+    return null;
+  }
+};
+
+module.exports = generateUrlReport;
---
+++ report/src/generateUrlReport.test.js
@@ -0,0 +1,107 @@
+// @ts-nocheck
+
+const fs = require("fs");
+const generateUrlReport = require("./generateUrlReport");
+
+jest.mock("fs");
+
+const testUrl = "https://www.test.com";
+const b64 = "aHR0cHM6Ly93d3cudGVzdC5jb20=";
+
+// mock a json in the latest scan
+const mockJson = (name, data) =>
+  jest.mock(
+    `results/${b64}/9876/${name}`,
+    () =>
+      data || {
+        data: name,
+      },
+    { virtual: true }
+  );
+
+// for some reason jest.resetModules doesnt work with virtual mocks
+const unMockJson = (name, data) => jest.unmock(`results/${b64}/9876/${name}`);
+
+describe("generateUrlReport", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.resetModules();
+  });
+
+  test(`should return null on invalid url`, () => {
+    expect(
+      generateUrlReport({
+        url: "https://www.invalid.com",
+      })
+    ).toEqual(null);
+  });
+
+  test(`should generate latest report for a valid url`, () => {
+    fs.existsSync.mockImplementationOnce(() => true); // check url folder
+    fs.existsSync.mockImplementationOnce(() => false); // screenshot
+    fs.readdirSync.mockImplementationOnce(() => ["9876", "1234", "5678"]); // assume scans folders use a sortable date-format
+
+    // mock all required files
+    mockJson("codescanalerts.json");
+    mockJson("dependabotalerts.json");
+    mockJson("http.json");
+    mockJson("lhr.json");
+    mockJson("nmapvuln.json");
+    mockJson("nuclei.json", []);
+    mockJson("testssl.json");
+    mockJson("thirdparties.json");
+    mockJson("updownio.json");
+    mockJson("wappalyzer.json");
+    mockJson("zap.json");
+
+    expect(
+      generateUrlReport({
+        url: testUrl,
+      })
+    ).toMatchSnapshot();
+
+    // unmock all required files
+    unMockJson("codescanalerts.json");
+    unMockJson("dependabotalerts.json");
+    unMockJson("http.json");
+    unMockJson("lhr.json");
+    unMockJson("nmapvuln.json");
+    unMockJson("nuclei.json", []);
+    unMockJson("testssl.json");
+    unMockJson("thirdparties.json");
+    unMockJson("updownio.json");
+    unMockJson("wappalyzer.json");
+    unMockJson("zap.json");
+
+
+  });
+  test(`should skip scans if some not found`, () => {
+    fs.existsSync.mockImplementationOnce(() => true); // check url folder
+    fs.existsSync.mockImplementationOnce(() => false); // screenshot
+    fs.readdirSync.mockImplementationOnce(() => ["9876", "1234", "5678"]); // assume scans folders use a sortable date-format
+
+    mockJson("codescanalerts.json");
+    mockJson("wappalyzer.json");
+
+    expect(
+      generateUrlReport({
+        url: testUrl,
+      })
+    ).toMatchSnapshot();
+
+    unMockJson("codescanalerts.json");
+    unMockJson("wappalyzer.json");
+    
+  });
+  
+  test(`should detect screenshot if any`, () => {
+    fs.existsSync.mockImplementationOnce(() => true); // check url folder
+    fs.existsSync.mockImplementationOnce(() => true); // screenshot
+    fs.readdirSync.mockImplementationOnce(() => ["9876", "1234", "5678"]); // assume scans folders use a sortable date-format
+    expect(
+      generateUrlReport({
+        url: testUrl,
+      }).screenshot
+    ).toEqual(true);
+  });
+});
