3b9e7ac709ce6e96c22153e297ed1fecf6aa75e2
Tristan Robert <tristan.robert.44@gmail.com>
2021-06-03 17:23:25 +0200

    refactor: depandabots sum repositories alerts and grade Fixes #9

--- report/src/summary/dependabot.js
+++ report/src/summary/dependabot.js
@@ -1,43 +1,10 @@
 /** @param {DependabotReport} report */
 const summary = (report) => {
   if (report) {
-    // dependabot
-    const dependabotCount =
-      report &&
-      report.filter &&
-      report
-        .filter(Boolean)
-        .map((repo) => repo.vulnerabilityAlerts.totalCount)
-        .reduce((prev, curr) => prev + curr, 0);
-    /**
-     * @param {"F" | "B" | "A"} a
-     * @param {"F" | "B" | "A"} b
-     */
-    const maxGrade = (a, b) => {
-      const grades = new Map();
-      grades.set("F", 3);
-      grades.set("B", 2);
-      grades.set("A", 1);
-      const orders = new Map();
-      orders.set(3, "F");
-      orders.set(2, "B");
-      orders.set(1, "A");
-      return orders.get(Math.max(grades.get(a), grades.get(b)));
+    return {
+      dependabotGrade: report.grade,
+      dependabotCount: report.totalCount
     };
-    const grades =
-      report &&
-      report.filter &&
-      report
-        .filter(Boolean)
-        .map((repo) => repo.grade);
-
-    const dependabotGrade = grades && grades.reduce(maxGrade);
-    if (dependabotGrade) {
-      return {
-        dependabotGrade,
-        dependabotCount
-      };
-    }
   }
 };
 
