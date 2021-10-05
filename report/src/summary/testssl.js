35ecd4874a142852f05684116d0026db20a52179
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-10-05 15:33:50 +0200

    feat(testssl): add expiration warning

--- report/src/summary/testssl.js
+++ report/src/summary/testssl.js
@@ -6,8 +6,35 @@ const summary = (report) => {
       report.find &&
       report.find((entry) => entry.id === "overall_grade");
     const value = overallGrade && overallGrade.finding;
+
+    const notAfterNode = report.find((r) => r.id === "cert_notAfter");
+    const notAfterIntermediateNode = report.find(
+      (r) => r.id === "intermediate_cert_notAfter <#1>"
+    );
+
+    // warn a month before
+    const warningDelay = 30 * 24 * 60 * 60 * 1000;
+    let expirationDate = null;
+    if (notAfterNode) {
+      if (notAfterIntermediateNode) {
+        const closest = Math.min(
+          new Date(notAfterNode.finding).getTime(),
+          new Date(notAfterIntermediateNode.finding).getTime()
+        );
+        expirationDate = closest;
+      } else {
+        expirationDate = new Date(notAfterNode.finding).getTime();
+      }
+    }
+
+    const expiresSoon =
+      expirationDate && new Date().getTime() + warningDelay > expirationDate;
+
     if (value) {
       return {
+        testsslExpireSoon: expiresSoon,
+        testsslExpireDate:
+          expirationDate && new Date(expirationDate).toISOString(),
         testsslGrade: value,
       };
     }
