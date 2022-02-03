e0b1e9c365b0e0aae93a8a632b3918d70c44e4ff
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-02-03 01:26:40 +0100

    typo

--- report/www/src/__tests__/Trends.test.js
+++ report/www/src/__tests__/Trends.test.js
@@ -1,14 +1,6 @@
-import path from "path";
 import * as React from "react";
 import { render } from "@testing-library/react";
-import Trends from "@/pages/Trends";
-
-const mockSampleConfig = JSON.parse(
-  jest
-    .requireActual("fs")
-    .readFileSync(path.join(__dirname, "config.json"))
-    .toString()
-);
+import Trends from "@/pages/trends";
 
 it("Should render Trends", () => {
   const { container } = render(<Trends />);
