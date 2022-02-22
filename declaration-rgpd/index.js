eebd76f9d17fe2b6a701bff12fc2373b5bb8f080
Clément <clement@numericite.eu>
2022-02-22 11:44:22 +0100

    feat: search for multiple links & refactor json output

--- declaration-rgpd/index.js
+++ declaration-rgpd/index.js
@@ -5,41 +5,32 @@ const { JSDOM } = jsdom;
 
 const searches = [
   {
-    needle: "Mentions légales",
+    slug: "ml",
+    needles: ["Mentions légales", "Legal Notice"],
   },
   {
-    needle: "Politique de confidentialité",
+    slug: "pc",
+    needles: ["Politique de confidentialité", "Privacy Policy"],
   },
 ];
 
 const analyseDom = async (dom, { url = "" } = {}) => {
   const text = dom.window.document.body.textContent;
   // fuzzy find the best match
-  const status = searches
-    .map(({ needle }) => ({ needle, score: fuzzy(needle, text) }))
-    .sort((a, b) => a.score - b.score)
-    .reverse();
-  // ensure were confident enough
-  const result = { mention: null };
-  const bestStatus = status[0];
-  if (bestStatus.score > 0.9) {
-    result.mention = bestStatus.needle;
-    // try to find related href if any
-    Array.from(dom.window.document.querySelectorAll("a")).filter((a) => {
-      if (fuzzy(bestStatus.needle, a.text) > 0.9) {
-        // make URL absolute when possible
-        const link = a.getAttribute("href");
-        if (link !== "#") {
-          const declarationUrl =
-            link.charAt(0) === "/" ? `${url || ""}${link}` : link;
-          result.declarationUrl = declarationUrl;
-        }
-      }
-    });
-    // loose href search
-    if (!result.declarationUrl) {
+  const results = [];
+  searches.forEach((search) => {
+    const result = { slug: search.slug, mention: null };
+    const status = search.needles
+      .map((needle) => ({ needle, score: fuzzy(needle, text) }))
+      .sort((a, b) => a.score - b.score)
+      .reverse();
+    // ensure were confident enough
+    const bestStatus = status[0];
+    if (bestStatus.score > 0.9) {
+      result.mention = bestStatus.needle;
+      // try to find related href if any
       Array.from(dom.window.document.querySelectorAll("a")).filter((a) => {
-        if (fuzzy("Accessibilité", a.text) > 0.9) {
+        if (fuzzy(bestStatus.needle, a.text) > 0.9) {
           // make URL absolute when possible
           const link = a.getAttribute("href");
           if (link !== "#") {
@@ -49,9 +40,24 @@ const analyseDom = async (dom, { url = "" } = {}) => {
           }
         }
       });
+      // loose href search
+      if (!result.declarationUrl) {
+        Array.from(dom.window.document.querySelectorAll("a")).filter((a) => {
+          if (fuzzy(search.needles[0], a.text) > 0.9) {
+            // make URL absolute when possible
+            const link = a.getAttribute("href");
+            if (link !== "#") {
+              const declarationUrl =
+                link.charAt(0) === "/" ? `${url || ""}${link}` : link;
+              result.declarationUrl = declarationUrl;
+            }
+          }
+        });
+      }
     }
-  }
-  return result;
+    results.push(result);
+  });
+  return results;
 };
 
 const analyseFile = async (filePath, { url } = {}) => {
