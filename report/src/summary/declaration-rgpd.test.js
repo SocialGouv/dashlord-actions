85f66363cdde76ac56e54fe1173fd016d91dfc8f
Clément <clement@numericite.eu>
2022-03-03 17:37:11 +0100

    feat: write a bunch of tests for rgpd module

--- report/src/summary/declaration-rgpd.test.js
+++ report/src/summary/declaration-rgpd.test.js
@@ -7,7 +7,6 @@ const tests = [
       {
         slug: "ml",
         declarationUrl: undefined,
-        expected: { "declaration-rgpd": "F" },
       },
       {
         slug: "pc",
@@ -15,6 +14,142 @@ const tests = [
       },
     ],
     expected: { "declaration-rgpd-ml": "F", "declaration-rgpd-pc": "F" },
+  },
+  {
+    name: "ML undefined & PC incomplete",
+    report: [
+      {
+        slug: "ml",
+        declarationUrl: undefined,
+      },
+      {
+        slug: "pc",
+        declarationUrl: "https://incomplete-pc.test",
+        maxScore: 4,
+        score: 3,
+      },
+    ],
+    expected: { "declaration-rgpd-ml": "F", "declaration-rgpd-pc": "D" },
+  },
+  {
+    name: "ML incomplete & PC undefined",
+    report: [
+      {
+        slug: "ml",
+        declarationUrl: "https://incomplete-ml.test",
+        maxScore: 4,
+        score: 3,
+      },
+      {
+        slug: "pc",
+        declarationUrl: undefined,
+      },
+    ],
+    expected: { "declaration-rgpd-ml": "D", "declaration-rgpd-pc": "F" },
+  },
+  {
+    name: "ML incomplete & PC incomplete",
+    report: [
+      {
+        slug: "ml",
+        declarationUrl: "https://incomplete-ml.test",
+        maxScore: 4,
+        score: 0,
+      },
+      {
+        slug: "pc",
+        declarationUrl: "https://incomplete-pc.test",
+        maxScore: 4,
+        score: 2,
+      },
+    ],
+    expected: { "declaration-rgpd-ml": "D", "declaration-rgpd-pc": "D" },
+  },
+  {
+    name: "ML valid & PC incomplete",
+    report: [
+      {
+        slug: "ml",
+        declarationUrl: "https://valid-ml.test",
+        maxScore: 4,
+        score: 4,
+      },
+      {
+        slug: "pc",
+        declarationUrl: "https://incomplete-pc.test",
+        maxScore: 4,
+        score: 2,
+      },
+    ],
+    expected: { "declaration-rgpd-ml": "A", "declaration-rgpd-pc": "D" },
+  },
+  {
+    name: "ML incomplete & PC valid",
+    report: [
+      {
+        slug: "ml",
+        declarationUrl: "https://incomplete-ml.test",
+        maxScore: 4,
+        score: 3,
+      },
+      {
+        slug: "pc",
+        declarationUrl: "https://valid-pc.test",
+        maxScore: 6,
+        score: 6,
+      },
+    ],
+    expected: { "declaration-rgpd-ml": "D", "declaration-rgpd-pc": "A" },
+  },
+  {
+    name: "ML valid & PC valid",
+    report: [
+      {
+        slug: "ml",
+        declarationUrl: "https://valid-ml.test",
+        maxScore: 3,
+        score: 3,
+      },
+      {
+        slug: "pc",
+        declarationUrl: "https://valid-pc.test",
+        maxScore: 1,
+        score: 1,
+      },
+    ],
+    expected: { "declaration-rgpd-ml": "A", "declaration-rgpd-pc": "A" },
+  },
+  {
+    name: "ML undefined & PC valid",
+    report: [
+      {
+        slug: "ml",
+        declarationUrl: undefined,
+      },
+      {
+        slug: "pc",
+        declarationUrl: "https://valid-pc.test",
+        maxScore: 1,
+        score: 1,
+      },
+    ],
+    expected: { "declaration-rgpd-ml": "F", "declaration-rgpd-pc": "A" },
+  },
+  {
+    name: "ML valid & PC undefined",
+    report: [
+      {
+        slug: "ml",
+        declarationUrl: "https://valid-ml.test",
+        maxScore: 1,
+        score: 1,
+      },
+      {
+        slug: "pc",
+        declarationUrl: undefined,
+      },
+    ],
+    expected: { "declaration-rgpd-ml": "A", "declaration-rgpd-pc": "F" },
   },
 ];
 
