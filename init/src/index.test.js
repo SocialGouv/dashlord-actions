a4742c8dcb6c26771f10f4919d793d8f95405b9f
Clément <clement@numericite.eu>
2022-03-04 11:21:19 +0100

    fix: update init test to match rgpd & a11y tests

--- init/src/index.test.js
+++ init/src/index.test.js
@@ -2,12 +2,15 @@ const fs = require("fs");
 const path = require("path");
 const core = require("@actions/core");
 
-const sampleConfig = jest.requireActual('fs').readFileSync(path.join(__dirname, "..", "dashlord.yml")).toString()
+const sampleConfig = jest
+  .requireActual("fs")
+  .readFileSync(path.join(__dirname, "..", "dashlord.yml"))
+  .toString();
 
 jest.mock("fs");
 
 const { getOutputs, getSiteTools } = require("./index");
- 
+
 let inputs = {};
 
 describe("should parse dashlord config", () => {
@@ -29,7 +32,7 @@ describe("should parse dashlord config", () => {
   });
 
   test("when single invalid url input", async () => {
-    inputs.url = 'zfzef'
+    inputs.url = "zfzef";
     fs.existsSync.mockReturnValue(true);
     fs.readFileSync.mockReturnValue(sampleConfig);
     const outputs = getOutputs();
@@ -37,28 +40,38 @@ describe("should parse dashlord config", () => {
   });
 
   test("when single valid url input", async () => {
-    inputs.url = 'https://www.free.fr'
+    inputs.url = "https://www.free.fr";
     fs.existsSync.mockReturnValue(true);
     fs.readFileSync.mockReturnValue(sampleConfig);
     const outputs = getOutputs();
     expect(outputs.sites).toMatchSnapshot();
   });
 
-    test("when multiple urls input", async () => {
-    inputs.url = 'https://www.free.fr,pouet,http://chez.com';
+  test("when multiple urls input", async () => {
+    inputs.url = "https://www.free.fr,pouet,http://chez.com";
     fs.existsSync.mockReturnValue(true);
     fs.readFileSync.mockReturnValue(sampleConfig);
     const outputs = getOutputs();
     expect(outputs.sites).toMatchSnapshot();
   });
 
-    test("and getSiteTools http://chez.com match", async () => {
-    inputs.url = 'http://chez.com';
+  test("and getSiteTools http://chez.com match", async () => {
+    inputs.url = "http://chez.com";
     fs.existsSync.mockReturnValue(true);
     fs.readFileSync.mockReturnValue(sampleConfig);
-    const tools = getSiteTools({"url":"http://chez.com","repositories":["chez/chez-ui","chez/chez-api"],"tools":{"screenshot":false,"updownio":false,"stats":false,"testssl":true}});
-    core.info(`tools=${JSON.stringify(tools)}`)
-    expect(tools).toEqual({screenshot: false,
+    const tools = getSiteTools({
+      url: "http://chez.com",
+      repositories: ["chez/chez-ui", "chez/chez-api"],
+      tools: {
+        screenshot: false,
+        updownio: false,
+        stats: false,
+        testssl: true,
+      },
+    });
+    core.info(`tools=${JSON.stringify(tools)}`);
+    expect(tools).toEqual({
+      screenshot: false,
       nmap: true,
       zap: true,
       wappalyzer: true,
@@ -70,8 +83,10 @@ describe("should parse dashlord config", () => {
       updownio: false,
       dependabot: true,
       codescan: true,
+      "declaration-a11y": true,
+      "declaration-rgpd": true,
       stats: false,
-      404: true});
+      404: true,
+    });
   });
-
 });
