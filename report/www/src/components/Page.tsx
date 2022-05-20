00c4bdecfc7b74203667064fdd8ec8123bbc1263
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-05-20 15:45:16 +0200

    fix(report): page alerts (#181)
    * fix(report): fix page alerts
    
    * snaps

--- report/www/src/components/Page.tsx
+++ report/www/src/components/Page.tsx
@@ -3,38 +3,37 @@ import { Alert } from "@dataesr/react-dsfr";
 
 type PageProps = { data: PageReport; url: string };
 
+const grades = {
+  A: "La page a bien été détectée à l'adresse standard : ",
+  B: "La page a bien été détectée à une adresse conforme : ",
+  C: "La page a bien été détectée, mais l'URL n'est pas conforme : ",
+};
+
 export const Page: React.FC<PageProps> = ({ data, url }) => {
+  const gradeMessage = grades[data.grade];
+
+  if (!gradeMessage) {
     return (
+      <Alert
+        type="error"
+        title=""
+        description="La page n'a pas été détectée!"
+      />
+    );
+  }
 
-        renderSuccessAlert()
-    )
-
-    function renderSuccessAlert() {
-        let text = "";
-
-        switch (data.grade) {
-            case "A":
-                text = "La page a bien été détectée à l'adresse standard : ";
-                break;
-            case "B":
-                text = "La page a bien été détectée à une adresse conforme : ";
-                break;
-            case "C":
-                text = "La page a bien été détectée, mais l'URL n'est pas conforme : ";
-                break;
-            default:
-                return <Alert
-                    type="error"
-                    title=""
-                    description="La page n'a pas été détectée!"
-                />
-
-        }
-        const urlLink = <a href={`${url}/${data.uri}`}>
+  return (
+    <Alert
+      type="success"
+      title=""
+      description={
+        <>
+          {gradeMessage}
+          <a href={`${url}/${data.uri}`}>
             {url}/{data.uri}
-        </a>
-        return <>{text}{urlLink}</>
-
-    }
-}
-
+          </a>
+        </>
+      }
+    />
+  );
+};
