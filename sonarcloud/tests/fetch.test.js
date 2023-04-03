2ae2ebe901775cb91ac6edebc37f910072dcb45a
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-04-03 09:21:44 +0200

    fix: tests

--- sonarcloud/tests/fetch.test.js
+++ sonarcloud/tests/fetch.test.js
@@ -1,7 +1,8 @@
 const generateJson = require("../src");
 
 test("get results from sonarcloud", async () => {
-  expect(
-    await generateJson(["sensgithub/eHospital", "zabbix/zabbix"])
-  ).toMatchSnapshot();
+  const result = await generateJson(["sensgithub/eHospital", "zabbix/zabbix"]);
+  expect(result.length).toEqual(2);
+  expect(result[0].result.status.vulnerabilities).toBeGreaterThan(0);
+  expect(result[1].result.status.vulnerabilities).toBeGreaterThan(0);
 });
