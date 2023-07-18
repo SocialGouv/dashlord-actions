6aa19ee3105eb81b8a81cbdf8850a8978dd3fa6b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-07-18 12:28:34 +0200

    fix(dependabot): dont count dismissed alerts (#271)

--- report/src/summary/dependabot.js
+++ report/src/summary/dependabot.js
@@ -1,9 +1,23 @@
+/**
+ *
+ * @param {number[]} arr
+ * @returns
+ */
+const sum = (arr) => arr.reduce((a, c) => a + c, 0);
+
 /** @param {DependabotReport} report */
 const summary = (report) => {
   if (report && report.grade) {
+    const totalCount = sum(
+      report.repositories.map(
+        (repo) =>
+          repo.vulnerabilityAlerts.nodes.filter((node) => !node.dismissedAt)
+            .length
+      )
+    );
     return {
       dependabotGrade: report.grade,
-      dependabotCount: report.totalCount
+      dependabotCount: totalCount,
     };
   }
 };
