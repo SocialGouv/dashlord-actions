f2cd9683700be1d7321de0865129671d18b425bf
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-02-01 22:42:29 +0100

    clean

--- report/www/src/__tests__/Footer.test.tsx
+++ report/www/src/__tests__/Footer.test.tsx
@@ -1,6 +1,5 @@
 import { render } from "@testing-library/react";
 import { FooterSite } from "@/components/FooterSite";
-import { vi } from "vitest";
 
 it("Should render FooterSite", () => {
   const { container } = render(<FooterSite />);
--- report/www/src/__tests__/Header.test.tsx
+++ report/www/src/__tests__/Header.test.tsx
@@ -1,6 +1,5 @@
 import { render } from "@testing-library/react";
 import { HeaderSite } from "@/components/HeaderSite";
-import { vi } from "vitest";
 
 it("Should render HeaderSite", () => {
   const { container } = render(<HeaderSite />);
