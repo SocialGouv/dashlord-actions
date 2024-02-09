25779f6247ce4d974383d7858b6d496d3f789203
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-02-09 10:22:57 +0100

    fix: tests

--- report/www/src/__tests__/UpdownIoDashboard.test.tsx
+++ report/www/src/__tests__/UpdownIoDashboard.test.tsx
@@ -1,5 +1,11 @@
 import { render } from "@testing-library/react";
 import UpdownIo from "../../pages/updownio";
+import { vi } from "vitest";
+
+const date = new Date(2021, 3, 6);
+
+vi.useFakeTimers();
+vi.setSystemTime(date);
 
 it("Should render UpdownIo", () => {
   const { container } = render(<UpdownIo />);
