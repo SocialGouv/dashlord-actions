576b26839c72a7ca4312a4ea4a4a8924e6c4b66a
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-05-06 23:26:26 +0200

    fix(report): drop custom performance for lighthouse metrics (#323)

--- report/src/summary/lighthouse.js
+++ report/src/summary/lighthouse.js
@@ -1,56 +1,11 @@
 const { scoreToGrade } = require("../utils");
 
-// compute a performance score from 0 to 100 from lighthouse report
-/**
- * @param {LighthouseReport} reportData
- *
- * @returns {number}
- */
-const getPerformanceScore = (reportData) => {
-  /* @type {LighthouseReport} */
-  if (!reportData) {
-    return 0;
-  }
-  const numRequests =
-    reportData.audits &&
-    reportData.audits.diagnostics &&
-    reportData.audits.diagnostics.details?.items &&
-    reportData.audits.diagnostics.details?.items[0].numRequests;
-  const totalByteWeight =
-    reportData.audits &&
-    reportData.audits.diagnostics &&
-    reportData.audits.diagnostics.details?.items &&
-    reportData.audits.diagnostics.details?.items[0].totalByteWeight;
-
-  const maxRequests = 50;
-  const maxByteWeight = 1024 * 1024;
-
-  let score = 100;
-
-  // penalty for additional requests : -5 per additionnal request
-  if (numRequests > maxRequests) {
-    score -= Math.min(100 / 2, (numRequests - maxRequests) * 5);
-  }
-  // penalty for big totalByteWeight : -20 per Mb
-  if (totalByteWeight > maxByteWeight) {
-    score -= Math.min(
-      100 / 2,
-      ((totalByteWeight - maxByteWeight) / (1024 * 1024)) * 20
-    );
-  }
-  score = Math.max(0, score / 100);
-  return score;
-};
-
 /** @param {LighthouseReport} report */
 const summary = (report) => {
   /* @type {LighthouseReport} */
   const reportData = (report && Array.isArray(report) && report[0]) || report; // use first lhr report
   if (reportData && reportData.categories) {
     const lhrCategories = reportData.categories;
-    if (lhrCategories["performance"]) {
-      lhrCategories["performance"].score = getPerformanceScore(reportData);
-    }
 
     return Object.keys(lhrCategories).reduce((scores, key) => {
       const score = lhrCategories[key].score;
